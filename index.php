4b0f5e21c8054e88202aa3b09099c6d2669d9ad0
javielcanche <jantonio.esquivels@gmail.com>
2016-04-21 20:20:56 -0600

    segundo cambio

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <a href='http://www.facebook.com'> <h3>entra a facebook</h3> </a><br>
 <a href='img/pasaje1.jpg'>abrir el paisaje </a><br><br>
 <p>Hola como estás???</p>
+<p>hola mundo</p>
 <img src='img/pasaje1.jpg'>
 </body>
 </html>
