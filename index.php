7a545cea363fc28f6676e0eda8cdf2e4a82994ca
javielcanche <jantonio.esquivels@gmail.com>
2016-04-21 19:48:37 -0600

    agregue

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title> Mi primer página </title>
+</head>
+<body>
+<!--aquí empieza el cuerpo de html-->
+<a href='http://www.facebook.com'> <h3>entra a facebook</h3> </a><br>
+<a href='img/pasaje1.jpg'>abrir el paisaje </a><br><br>
+<img src='img/pasaje1.jpg'>
+</body>
+</html>
